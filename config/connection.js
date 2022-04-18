14082aa3be3623812ee243e0ca99b8cce97eb97b
Rashid <rasvpz@gmail.com>
2022-04-18 15:06:59 +0530

    db issues

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,9 @@ const state={
 module.exports.connect=function(done){
     const url = 'mongodb+srv://bigoAtlas:W5QMMf2WtSU23XGD@bigo.tpoon.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     const dbname ='bigOproject'
+
     MongoClient.connect(url,(err, data)=>{
+        if(err)return done(err)
         state.db=data.db(dbname)
         done()
     })    
